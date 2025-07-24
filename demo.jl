59be1a2dafdafeeed85a265c3e76554488ac42cd
Rachel Kurchin <rkurchin@cmu.edu>
2025-07-24 15:29:52 -0400

    finish demo file
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- demo.jl
+++ demo.jl
@@ -2,7 +2,11 @@ using Nodariety
 using MetaGraphs
 using Graphs
 
-n = 77 # pick a number, this would be Dirac
+# pick a number, a few possibilities below...
+n = 77 # this would be Dirac
+# n = 36 # Cauchy
+# n = 45 # Cayley
+# n = 338 # Riemann
 
 props(hg.graph, n) # get info about this node
 
@@ -23,10 +27,16 @@ subgraph.node_info.family_name
 # centrality?
 all_centrals()
 
+# cycles?
+cycs = simplecycles(hg.graph)
+get_prop.(Ref(hg.graph), cycs, Ref(:family_name))
+
 # demographics (if time)
-# ...
+node_histogram("birth_country")
 
-# graph viz
-# pick some colorcodings to try etc.
+# graph viz, etc.
+clus = get_clusters()
+g2 = induced_subgraph(hg, clus[1])[1]
+plot_graph(g2, node_color_prop="birth_year", edge_color_prop="year")
 
-# then show website
+# then show website: https://rkurchin.github.io/nodarietyvis/
